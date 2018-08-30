3b622bbd5203a77fdbaf27c4afd303f91c203b53
unknown <lianlong@rong360ad.com>
2018-08-30 14:23:33 +0800

    mast test

--- 111.py
+++ 111.py
@@ -1,11 +1,7 @@
-print 44444444444
 print 3423424242
 print 3423424242
-print 44444444444
-print 44444444444
 print 44444444444
 print "xiaohong"
 print 44444444444
-print "lianlong"
 print "lianlong"
 print "lianlong"
